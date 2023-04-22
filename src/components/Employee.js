39a7f5de978a746e8da4d567de7f0da766db2bd9
Jihun Park <jihunpark0989@gmail.com>
2023-04-22 20:23:27 +0100

    Used state variable for EditEmployee component ([name, setName] and [role, setRole]) so when changes occur to the role, we use the state variable set function to update the state variable

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -4,7 +4,7 @@ function Employee(props)
 {
     return (
         <>
-            <div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
                 <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} />
                 <div className="text-center space-y-2 sm:text-left">
                     <div className="space-y-0.5">
@@ -15,7 +15,7 @@ function Employee(props)
                         {props.role ? props.role: 'no role'}
                         </p>
                     </div>
-                    <EditEmployee />
+                    <EditEmployee name={props.name} role={props.role}/>
                 </div>
             </div>
         </>
