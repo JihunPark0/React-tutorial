2c91a74ec9afc73a819d7da5e3582111bd9ae889
Jihun Park <jihunpark0989@gmail.com>
2023-04-22 18:33:50 +0100

    Using react-bootstrap modal component from the documentation, created a pop up modal component and added it to the employee component

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,3 +1,5 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props)
 {
     return (
@@ -13,6 +15,7 @@ function Employee(props)
                         {props.role ? <p>{props.role}</p> : <p>no role</p>}
                         </p>
                     </div>
+                    <EditEmployee />
                     <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
                 </div>
             </div>
