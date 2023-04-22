5d73106f5ea8e4537541b68a6534647fc45dbf20
Jihun Park <jihunpark0989@gmail.com>
2023-04-22 12:26:54 +0100

    created first component named Employee

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
+import Employee from './components/Employee';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Employee />
+      <Employee />
+      <Employee />
+      <Employee />
+      <Employee />
     </div>
   );
 }
