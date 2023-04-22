99c5a95304da64bcaea16fbefcb43182c2814831
Jihun Park <jihunpark0989@gmail.com>
2023-04-22 13:11:35 +0100

    Using tailwind documentation, addded documentation to employee and app components to apply ready made card styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {useState} from 'react'
 function App() {
 //states are different to variables
 //Because the state can be tied to the user interface so that when the 'state' changes the user interface will automatically update w/o a page refresh
-const [role, setRole] = useState('dev');//example of using useState [variable, function to set the variable always prefixed with set..]
+const [role, setRole] = useState('Developer');//example of using useState [variable, function to set the variable always prefixed with set..]
 //number one rule of using state: never assign a value to the variable directly
 //YOU ALWAYS GO THROUGH setter
 //HOOK?
@@ -14,19 +14,21 @@ const [role, setRole] = useState('dev');//example of using useState [variable, f
 //there are other hooks that we can use to introduce functionality into our components very easily
 //usually hooks are prefixed with 'use'
 const showEmp = true;
+
+ //used pexel images for free images
   return (
     <div className="App">
       {showEmp ? (
         <>
-          <input type='text' onChange={(e)=>{
-            console.log(e.target.value);
-            setRole(e.target.value);
-            }}/>
-          <Employee name="Jihun" role="Junior Developer"/>
-          <Employee name="Daniel" role={role}/>
-          <Employee />
-          <Employee />
-          <Employee />
+          <div className="flex flex-wrap justify-center"> 
+            <Employee name="Jihun" role="Junior Developer" img="https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg"/>
+            <Employee name="Daniel" role={role} img="https://images.pexels.com/photos/11140270/pexels-photo-11140270.jpeg"/>
+            <Employee name="Jihun" role="Junior Developer" img="https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg"/>
+            <Employee name="Daniel" role={role} img="https://images.pexels.com/photos/11140270/pexels-photo-11140270.jpeg"/>
+            <Employee name="Jihun" role="Junior Developer" img="https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg"/>
+            <Employee name="Daniel" role={role} img="https://images.pexels.com/photos/11140270/pexels-photo-11140270.jpeg"/>
+
+          </div>
         </>
       ) : (
         <p>You can't see the employees</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,9 +2,20 @@ function Employee(props)
 {
     return (
         <>
-            <h3 className="text-3xl font-bold underline">Employee {props.name}</h3>
-            <p>{props.role ? props.role : 'No role'}</p>
-            {props.role ? <p>{props.role}</p> : <p>No roll</p>}
+            <div class="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} />
+                <div class="text-center space-y-2 sm:text-left">
+                    <div class="space-y-0.5">
+                        <p class="text-lg text-black font-semibold">
+                        {props.name}
+                        </p>
+                        <p class="text-slate-500 font-medium">
+                        {props.role ? <p>{props.role}</p> : <p>no role</p>}
+                        </p>
+                    </div>
+                    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Update</button>
+                </div>
+            </div>
         </>
     )
 }
