fe395fa518906e1d7e0cdd6236eceb08ae3fb6a2
Jihun Park <jihunpark0989@gmail.com>
2023-04-23 22:04:29 +0100

    Cleaned and formatted code for readability

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function AddEmployee(props) {
-  const [name, setName] = useState('');
-  const [role, setRole] = useState('');
-  const [img, setImg] = useState('');
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [img, setImg] = useState("");
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -13,7 +13,12 @@ function AddEmployee(props) {
 
   return (
     <>
-    <button onClick={handleShow} className="m-2 block mx-auto bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">+ Add Employee</button>
+      <button
+        onClick={handleShow}
+        className="m-2 block mx-auto bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+      >
+        + Add Employee
+      </button>
 
       <Modal
         show={show}
@@ -25,64 +30,101 @@ function AddEmployee(props) {
           <Modal.Title>Add employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form onSubmit={(e)=>{e.preventDefault(); setName(''); setRole(''); setImg(''); props.newEmployee(name, role, img);  console.log("hello from editemployee")}} id="editmodel" class="w-full max-w-sm">
-        <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
-                    Full Name
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setName("");
+              setRole("");
+              setImg("");
+              props.newEmployee(name, role, img);
+              console.log("hello from editemployee");
+            }}
+            id="editmodel"
+            class="w-full max-w-sm"
+          >
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="name"
+                >
+                  Full Name
                 </label>
-            </div>
-            <div class="md:w-2/3">
-                <input 
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="name" 
-                    type="text" 
-                    value={name}
-                    onChange={(e)=>{setName(e.target.value)}}
-                    placeholder='Jihun Park'
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  placeholder="Jihun Park"
                 />
+              </div>
             </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="role">
-                    Role
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="role"
+                >
+                  Role
                 </label>
-            </div>
-            <div class="md:w-2/3">
-                <input 
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="role" 
-                    type="text" 
-                    value={role}
-                    onChange={(e)=>{setRole(e.target.value)}}
-                    placeholder='Bank teller'
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="role"
+                  type="text"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
+                  placeholder="Bank teller"
                 />
+              </div>
             </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="img">
-                    Image URL
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="img"
+                >
+                  Image URL
                 </label>
-            </div>
-            <div class="md:w-2/3">
-                <input 
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
-                    id="img" 
-                    type="text" 
-                    value={img}
-                    onChange={(e)=>{setImg(e.target.value); console.log(e)}}
-                    placeholder='https://google.com'
+              </div>
+              <div class="md:w-2/3">
+                <input
+                  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="img"
+                  type="text"
+                  value={img}
+                  onChange={(e) => {
+                    setImg(e.target.value);
+                    console.log(e);
+                  }}
+                  placeholder="https://google.com"
                 />
+              </div>
             </div>
-        </div>
-        </form>
-
+          </form>
         </Modal.Body>
         <Modal.Footer>
-            <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" onClick={handleClose}>Close</button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClose} form="editmodel">Add</button>
+          <button
+            className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleClose}
+          >
+            Close
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleClose}
+            form="editmodel"
+          >
+            Add
+          </button>
         </Modal.Footer>
       </Modal>
     </>
