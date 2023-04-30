e495c8400cab684ccc8054420a97659a06ab6d8e
Jihun Park <jihunpark0989@gmail.com>
2023-04-30 10:26:15 +0100

    Routing with React Router; Installed React Router and using BrowserRouter, Router and Route and attached a URL path to our page component to be rendered using path attribute and element attribute

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./index.css";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Customers from "./pages/Customers";
 function App() {
   return (
     <Header>
-      <Employees />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Employees" element={<Employees />}></Route>
+          <Route path="/customers" element={<Customers />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Header>
   );
 }
