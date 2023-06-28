db4acd7fc305e7aed1ea0a2eb97c922b57521d32
Jihun Park <jihunpark0989@gmail.com>
2023-06-28 20:01:18 +0100

    enabled edit customers by using a django endpoint to POST request to update customer details, also enabled canceling changes by have a tempCustomer state and customer state

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -5,10 +5,21 @@ import { baseUrl } from "../shared";
 
 export default function Customer() {
   const { id } = useParams();
+
   const [customer, setCustomer] = useState();
+  const [tempCustomer, setTempCustomer] = useState();
+  const [changed, setChanged] = useState(false);
+
   const navigate = useNavigate();
   const [notFound, setNotfound] = useState();
   const url = baseUrl + "/api/customers/" + id;
+
+  useEffect(() => {
+    console.log("customer: ", customer);
+    console.log("tempCustomer: ", tempCustomer);
+    console.log(changed);
+  });
+
   useEffect(() => {
     fetch(url)
       .then((response) => {
@@ -23,17 +34,66 @@ export default function Customer() {
       })
       .then((data) => {
         setCustomer(data.customer);
+        setTempCustomer(data.customer);
       });
   }, []);
+  function updateCustomer() {
+    const url = baseUrl + "/api/customers/" + id;
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tempCustomer),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //success case
+        //this data should be the new customer state
+        setCustomer(data.customer);
+        //to allow persistent layer is reflected on the state variable
+        setChanged(false);
+        console.log(data);
+      })
+      .catch(() => {});
+  }
 
   return (
     <>
       {notFound ? <NotFound id={id} /> : null}
       {customer ? (
         <div>
-          <p>{customer.id}</p>
-          <p>{customer.name}</p>
-          <p>{customer.industry}</p>
+          <input
+            className="block m-2 px-2"
+            type="text"
+            value={tempCustomer.name}
+            onChange={(e) => {
+              setTempCustomer({ ...tempCustomer, name: e.target.value });
+              setChanged(true);
+            }}
+          />
+          <input
+            className="block m-2 px-2"
+            type="text"
+            value={tempCustomer.industry}
+            onChange={(e) => {
+              setTempCustomer({ ...tempCustomer, industry: e.target.value });
+              setChanged(true);
+            }}
+          />
+          {changed ? (
+            <>
+              <button
+                onClick={() => {
+                  setTempCustomer({ ...customer });
+                  setChanged(false);
+                }}
+              >
+                Cancel
+              </button>
+              <button onClick={updateCustomer}>Save</button>
+            </>
+          ) : null}
         </div>
       ) : null}
       <button
