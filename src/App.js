b8c05a2390d38c1090b0be201cb215b472d182e2
Jihun Park <jihunpark0989@gmail.com>
2023-07-10 21:41:23 +0100

    Using a setInterval function, I have set it so that if the user is logged in, it will request for a refresh token from the server api/token/refresh every three minutes using the first refresh token stored in local storage when user logged in. This was done to prevent users having to log back in every time the access token expires every 15 minutes. This was achieved by requesting for a refresh token every interval that is smaller than the time it takes for the access token to expire. This therefore allows for a seemless experience

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,14 +9,42 @@ import Definition from "./components/Definition";
 import NotFound from "./components/NotFound";
 import Customer from "./pages/Customer";
 import Login from "./pages/Login";
+import { baseUrl } from "./shared";
 
 export const LoginContext = createContext();
 
 function App() {
   //Shrot term-> Default value below. Check localStorage for an access token
   //Long term goal -> use Refresh token and if it works, stay logged in, otherwise, send to login page
-  const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
+  const minute = 1000 * 60;
 
+  const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
+  useEffect(() => {
+    function refreshTokens() {
+      if (localStorage.refresh) {
+        const url = baseUrl + "/api/token/refresh/";
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          }),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            localStorage.access = data.access;
+            localStorage.refresh = data.refresh;
+          });
+      }
+    }
+    refreshTokens();
+    setInterval(refreshTokens, minute * 3);
+  }, []);
   function changeLoggedIn(value) {
     setLoggedIn(value);
     if (value === false) {
